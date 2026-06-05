f2e46c51f283c232b37f8d89799472dd475937c5
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-06-04 21:33:29 -0400

    feat: se implementa PacienteRepository con metodos de busqueda personalizados

---
+++ src/main/java/com/grupo10/patient_service/repository/PatientRespository.java
@@ -0,0 +1,14 @@
+package com.grupo10.patient_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.grupo10.patient_service.model.Patient;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface PatientRespository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByIdUsuario(Long idUsuario);
+    Optional<Patient> findByRut(String rut);
+
+    boolean existsByRut(String rut);
+}
