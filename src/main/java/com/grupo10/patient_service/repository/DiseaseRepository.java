961c2d00ebcbb69df7303dbeba36fd795e6f983d
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-06-26 20:01:05 -0400

    refactor: se elimina anotacion @Respository redundante

--- src/main/java/com/grupo10/patient_service/repository/DiseaseRepository.java
+++ src/main/java/com/grupo10/patient_service/repository/DiseaseRepository.java
@@ -2,8 +2,6 @@ package com.grupo10.patient_service.repository;
 
 import com.grupo10.patient_service.model.Disease;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DiseaseRepository extends JpaRepository<Disease, Long> {
 }
