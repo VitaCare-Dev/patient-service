fdc880114e681287359bcfd0de81ce8d86fdcfb9
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-06-26 20:31:46 -0400

    refactor: se agrega constante

--- src/main/java/com/grupo10/patient_service/constants/GlobalConstants.java
+++ src/main/java/com/grupo10/patient_service/constants/GlobalConstants.java
@@ -7,6 +7,7 @@ public class GlobalConstants {
 
     public static final String PATIENT_NOT_FOUND = "Paciente no encontrado con el ID: ";
     public static final String DUPLICATE_RUT = "Ya existe un paciente registrado con el RUT ingresado";
+    public static final String DUPLICATE_USER_PATIENT = "El usuario ya tiene un paciente registrado";
     public static final String ADDRESS_NOT_FOUND = "Dirección no encontrada con el ID: ";
     public static final String DISEASE_NOT_FOUND = "Enfermedad no encontrada con el ID: ";
 }
